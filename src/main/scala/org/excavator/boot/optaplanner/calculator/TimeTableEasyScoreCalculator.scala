6b99c39049fdfb9499083fd931014ed85e023911
CMonkey <42.codemonkey@gmail.com>
2020-10-13 15:39:05 +0800

    override calculateScore function to TimeTableEasyScoreCalculator

--- src/main/scala/org/excavator/boot/optaplanner/calculator/TimeTableEasyScoreCalculator.scala
+++ src/main/scala/org/excavator/boot/optaplanner/calculator/TimeTableEasyScoreCalculator.scala
@@ -1,11 +1,38 @@
 package org.excavator.boot.optaplanner.calculator
 
 import org.excavator.boot.optaplanner.domain.TimeTable
-import org.optaplanner.core.api.score.Score
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator
 
 class TimeTableEasyScoreCalculator extends EasyScoreCalculator[TimeTable] {
-  override def calculateScore(solution: TimeTable): Score[_ <: Score[_ <: AnyRef]] = {
+  override def calculateScore(timeTable: TimeTable): HardSoftScore = {
+
+    val lessonList = timeTable.getLessonList
+
+    var hardScore = 0
+
+    for (a <- lessonList){
+      for (b <- lessonList){
+        if(a.getTimeslot != null && a.getTimeslot.equals(b.getTimeslot) && a.getId < b.getId){
+          if (a.getRoom != null && a.getRoom.equals(b.getRoom)){
+            hardScore += 1
+          }
+
+          if(a.getTeacher.equals(b.getTeacher)){
+            hardScore -= 1
+          }
+
+          if(a.getStudentGroup.equals(b.getStudentGroup)){
+            hardScore -= 1
+          }
+        }
+
+      }
+    }
+
+    val softScore = 0
+
+    HardSoftScore.of(hardScore, softScore)
 
   }
 }
