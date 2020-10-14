dca93851e9bfe0e662ad114f5cc677fe67121400
CMonkey <42.codemonkey@gmail.com>
2020-10-14 09:46:39 +0800

    delete Timeslot.scala

--- src/main/scala/org/excavator/boot/optaplanner/domain/Timeslot.scala
+++
@@ -1,8 +0,0 @@
-package org.excavator.boot.optaplanner.domain
-
-import java.time.{DayOfWeek, LocalTime}
-
-case class Timeslot(dayOfWeek: DayOfWeek, startTime: LocalTime, endTime: LocalTime) {
-  override def toString() = dayOfWeek + " " + startTime.toString()
-
-}
